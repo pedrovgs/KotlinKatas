2158bfe6a1a168bbe901886c6489ee4c37e72828
Antonio López Marín <tonibasket_p.c@hotmail.com>
2019-01-22 10:41:17 +0100

    Remove return statements (#2)
    * Remove return statements
    
    * Fix ktlint

--- src/main/kotlin/com/github/pedrovgs/maxibons/KarumiHQs.kt
+++ src/main/kotlin/com/github/pedrovgs/maxibons/KarumiHQs.kt
@@ -15,18 +15,16 @@ class KarumiHQs<F>(private val M: Monad<F>, private val chat: Chat<F>) {
     fun openFridge(world: World, developers: List<Developer>): Kind<F, World> =
             developers.foldM(M, world, this::openFridge)
 
-    fun openFridge(world: World, developer: Developer): Kind<F, World> {
-        return M.binding {
-            val finalMaxibonsAmount = refillMaxibonsIfNeeded(developer, computeMaxibonsLeft(world, developer)).bind()
-            World.worldMaxibonsLeft.set(world, finalMaxibonsAmount)
-        }
+    fun openFridge(world: World, developer: Developer): Kind<F, World> = M.binding {
+        val finalMaxibonsAmount = refillMaxibonsIfNeeded(developer, computeMaxibonsLeft(world, developer)).bind()
+        World.worldMaxibonsLeft.set(world, finalMaxibonsAmount)
     }
 
     private fun computeMaxibonsLeft(world: World, developer: Developer): Int =
             Math.max(0, world.karumiFridge.maxibonsLeft - developer.maxibonsToGrab)
 
-    private fun refillMaxibonsIfNeeded(developer: Developer, maxibonsLeft: Int): Kind<F, Int> {
-        return if (shouldRefillMaxibons(maxibonsLeft)) {
+    private fun refillMaxibonsIfNeeded(developer: Developer, maxibonsLeft: Int): Kind<F, Int> =
+        if (shouldRefillMaxibons(maxibonsLeft)) {
             M.binding {
                 askTheTeamForMoreMaxibons(developer).bind()
                 maxibonsLeft + maxibonsToRefill
@@ -34,11 +32,9 @@ class KarumiHQs<F>(private val M: Monad<F>, private val chat: Chat<F>) {
         } else {
             M.just(maxibonsLeft)
         }
-    }
 
     private fun shouldRefillMaxibons(maxibonsLeft: Int) = maxibonsLeft <= minNumberOfMaxibons
 
-    private fun askTheTeamForMoreMaxibons(developer: Developer): Kind<F, String> {
-        return chat.sendMessage("Hi there! I'm ${developer.name}. We need more maxibons")
-    }
+    private fun askTheTeamForMoreMaxibons(developer: Developer): Kind<F, String> =
+        chat.sendMessage("Hi there! I'm ${developer.name}. We need more maxibons")
 }
