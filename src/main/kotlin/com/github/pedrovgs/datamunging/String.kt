e6cf116d253f6b033cb02f8242b417518d2b51a0
Pedro Vicente Gómez Sánchez <pedro@karumi.com>
2019-11-10 23:00:54 +0100

    Remove kleisi and add answers to the kata questions

--- src/main/kotlin/com/github/pedrovgs/datamunging/String.kt
+++ src/main/kotlin/com/github/pedrovgs/datamunging/String.kt
@@ -3,20 +3,9 @@ package com.github.pedrovgs.datamunging
 import arrow.core.None
 import arrow.core.Option
 import arrow.core.Some
-import arrow.core.fix
-import arrow.mtl.Kleisli
 
-private val optionStringToIntKleisli = Kleisli { str: String ->
-    if (str.toCharArray().all { it.isDigit() }) Some(str.toInt()) else None
-}
-private val optionStringToDoubleKleisli = Kleisli { str: String ->
-    if (str.toCharArray().all { it.isDigit() }) Some(str.toDouble()) else None
-}
+fun String.safeToInt(): Option<Int> =
+        if (this.isNotEmpty() && this.toCharArray().all { it.isDigit() }) Some(this.toInt()) else None
 
-fun String.safeToInt(): Option<Int> {
-    return optionStringToIntKleisli.run(this).fix()
-}
-
-fun String.safeToDouble(): Option<Double> {
-    return optionStringToDoubleKleisli.run(this).fix()
-}
+fun String.safeToDouble(): Option<Double> =
+        if (this.isNotEmpty() && this.toCharArray().all { it.isDigit() }) Some(this.toDouble()) else None
--- src/main/kotlin/com/github/pedrovgs/datamunging/main.kt
+++ src/main/kotlin/com/github/pedrovgs/datamunging/main.kt
@@ -16,3 +16,27 @@ fun main() {
         Pair(weather, team)
     }.unsafeRunSync()
 }
+
+/*
+ * Kata Questions
+ * To what extent did the design decisions you made when writing the original programs make it
+ * easier or harder to factor out common code?
+ *
+ * Making both programs returning a value instead of an Option<T> made harder the final
+ * implementation because this makes no sense at all.
+ *
+ * However, using functional combinators and grouping the code related to the file read process
+ * made the final refactor way easier.
+ *
+ * Was the way you wrote the second program influenced by writing the first?
+ *
+ * Nope, but it helped me to finalize the final design.
+ *
+ * Is factoring out as much common code as possible always a good thing? Did the readability of the programs suffer because of this requirement? How about the maintainability?
+ *
+ * Refactoring after the second usage was the best choice. We can't create the correct adapter if
+ * we don't know how the code has to be used at least twice.
+ *
+ * We could move more code to the common abstraction because these two programs are designed
+ * to find the minimum value for any param but I think this will reduce flexibility in the future.
+ */
